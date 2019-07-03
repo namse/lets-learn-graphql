79ed83d5471efa728755b9efe11acf6171441c7f
남세현 <namse@bluehole.net>
2019-07-03 12:41:12 +0900

    Add comment for skipable resolver

--- server/graphql/schema_1_GraphQLSchema.js
+++ server/graphql/schema_1_GraphQLSchema.js
@@ -12,18 +12,25 @@ const UserType = new GraphQLObjectType({
   fields: {
     id: {
       type: GraphQLInt,
-      resolve(obj, args, context, resolveInfo) {
+      resolve(obj, args, context, resolveInfo) { // We don't need this reoslver => read #1
         return obj.id;
       },
     },
     username: {
       type: GraphQLString,
-      resolve(obj, args, context, resolveInfo) {
+      resolve(obj, args, context, resolveInfo) { // We don't need this reoslver => read #1
         return obj.username;
       },
     }
   }
 });
+
+/// #1
+/// If resolver function just return obj[field], we don't have to provide resolver function!
+/// 1. In Type Definition if field has resolver, they call resolver function and take return value.
+/// 2. In Type Definition if field doesn't have resolver, they try to access to obj[field].
+/// 2.1. if obj[field] is function, graphql calls it and take return value.
+/// 2.2. If obj[field] is value, they just use that.
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
